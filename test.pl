4e1548b8344b1e5aa790de4bfcb209f8567503a6
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-06-14 18:55:29 -0300

    Date::Calc::Iterator test

--- test.pl
+++ test.pl
@@ -5,9 +5,11 @@ use 5.22.1;
 use strict;
 use warnings;
 use LWP::Simple;
+use Date::Calc::Iterator;
 
 BEGIN {
         die "Antes de executar, instale o modulo LWP::Simple com o comando:\ncpan LWP::Simple\n" unless (eval{require LWP::Simple});
+	die "Antes de executar, instale o modulo Date::Calc::Iterator com o comando:\ncpan Date::Calc::Iterator\n" unless (eval{require Date::Calc::Iterator});
 }
 
 print "\n============================== scrpr4dou ==============================\n";
@@ -26,8 +28,6 @@ if ($file =~ /(&totalArquivos=)([0-9]+)"/) {
 	say "$2 pages";
 }
 
-=cut
-
 sub check_dou_filetype {
         my $uri = shift(@_);
         my @fl = head($uri);
@@ -39,3 +39,10 @@ sub check_dou_filetype {
 my $url = "http://pesquisa.in.gov.br/imprensa/servlet/INPDFViewer?jornal=10&pagina=1&data=14/06/2016&captchafield=firistAccess";
 my $pth = "dou1pg1_2016_06_14.pdf";
 check_dou_filetype($url) ? getstore($url, $pth) && say "Salvando arquivo PDF $pth" : say "Arquivo $pth nao sera salvo pois nao se trata de um PDF";
+
+=cut
+
+my $i1 = Date::Calc::Iterator->new(from => [2016,06,01], to => [2016,06,14]);
+my @dates1;
+push @dates1,$_ while $_ = $i1->next;
+print join("\n", @dates1);

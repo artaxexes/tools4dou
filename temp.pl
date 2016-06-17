05e5d080fe9efb5d3cabbfc0355546b159cc4038
Artaxexes J. Ferreira <artaxexes@gmail.com>
2016-06-17 18:54:27 -0300

    More tests

--- temp.pl
+++ temp.pl
@@ -2,9 +2,9 @@
 # test.pl
 
 BEGIN {
-	die "Modulo DateTime necessario, instale-o\n" unless (eval{require DateTime});
-	die "Modulo LWP::Simple necessario, instale-o\n" unless (eval{require LWP::Simple});
-	die "Modulo Date::Calc::Iterator necessario, instale-o\n" unless (eval{require Date::Calc::Iterator});
+	die "Install DateTime" unless (eval{require DateTime});
+	die "Install LWP" unless (eval{require LWP});
+	die "Install PDF" unless (eval{require PDF});
 }
 
 use 5.22.1;
@@ -12,60 +12,61 @@ use strict;
 use warnings;
 use LWP::Simple;
 use DateTime;
-use Date::Calc::Iterator;
+use PDF;
 
-=pod
-
-# my $url = "http://pesquisa.in.gov.br/imprensa/jsp/visualiza/index.jsp?jornal=1&pagina=1&data=10/06/2016&captchafield=firistAccess";
-# my $url = "http://pesquisa.in.gov.br/imprensa/servlet/INPDFViewer?jornal=10&pagina=1&data=14/06/2016&captchafield=firistAccess";
-
-=cut
+my $url = "http://pesquisa.in.gov.br/imprensa/jsp/visualiza/index.jsp?jornal=1&pagina=1&data=10/06/2016&captchafield=firistAccess";
+$url = "http://pesquisa.in.gov.br/imprensa/servlet/INPDFViewer?jornal=2&pagina=1&data=17/06/2016&captchafield=firistAccess";
 
 test04();
 
 sub test01 {
-        my $url = shift;
-        my @info = head($url);
-        return 0 unless ($info[0] eq "application/pdf");
-        return 1;
-}
-
-sub test02 {
-	my $url = shift;
-	my $file = get($url);
-	if ($file =~ /(&totalArquivos=)([0-9]+)"/) {
-		say "$2 pages";
+	my $ua = LWP::UserAgent->new;
+	$ua->timeout(10);
+	my $response = $ua->get($url);
+	if ($response->is_success) {
+		print $response->decoded_content;
+	}
+	else {
+		die $response->status_line;
 	}
 }
 
+=pod
+sub test02 {
+	my $pdf=PDF->new ;
+	$pdf=PDF->new(filename);
+	my $result=$pdf->TargetFile( filename );
+	print "is a pdf file\n" if ( $pdf->IsaPDF ) ;
+	print "Has ",$pdf->Pages," Pages \n";
+	print "Use a PDF Version  ",$pdf->Version ," \n";
+	print "and it is crypted  " if ( $pdf->IscryptedPDF) ;
+	print "filename with title",$pdf->GetInfo("Title"),"\n";
+	print "and with subject ",$pdf->GetInfo("Subject"),"\n";
+	print "was written by ",$pdf->GetInfo("Author"),"\n";
+	print "in date ",$pdf->GetInfo("CreationDate"),"\n";
+	print "using ",$pdf->GetInfo("Creator"),"\n";
+	print "and converted with ",$pdf->GetInfo("Producer"),"\n";
+	print "The last modification occurred ",$pdf->GetInfo("ModDate"),"\n";
+	print "The associated keywords are ",$pdf->GetInfo("Keywords"),"\n";
+	my ($startx,$starty, endx,endy) = $pdf->PageSize ($page) ;
+}
+=cut
+
 sub test03 {
-	my (@date_begin, @date_final) = @_;
-	my $i = Date::Calc::Iterator->new(from => [@date_begin], to => [@date_final]);
-	#my $i = Date::Calc::Iterator->new(from => [2016,06,01], to => [2016,06,14]);
-	my @dts;
-	push(@dts,$_) while $_ = $i->next;
-	foreach my $dt (@dts) {
-		print sprintf("%02d/%02d/%04d\n", @{$dt}[2], @{$dt}[1], @{$dt}[0]);
+	my $string = 'EXONERAR(, a pedido,)?';
+	my $file = 'dou.pdf';
+	my @results_search = `pdftotext $file - | grep --basic-regexp \"$string\"`;
+	my $result_count = scalar @results_search;
+	say "$result_count results founded";
+	foreach my $result (@results_search) {
+		print $result;
 	}
 }
 
 sub test04 {
-	my $date1 = DateTime->new(year => '2016', month => '06', day => '01');
-	my $date2 = DateTime->new(year => '2016', month => '06', day => '15');
-	my ($date, $date_begin, $date_final);
-	while($date1 <= $date2) {
-		$date = $date1->dmy('/');
-		$date1->add(days => 1);
-	}
-	$date = 'all';
-	(($date_begin = '02/01/1990') && ($date_final = $date2->dmy('/'))) if ($date eq 'all');
-	my $test = DateTime->new(year => (substr $date_begin, 6, 4), month => (substr $date_begin, 3, 2), day => (substr $date_begin, 0, 2));
-	say $test->dmy('/');
-	#say $date_begin;
-	#say $date_final;
-	for ($date) {
-		(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/ || /today/) and do { say '15/06/2016'; last; };
-		(/nothing/ || /empty/) and do { say 'nothing or empty'; last; };
-		say 'default';
-	}
+	my ($a, $b, $c);
+	$a = $b = $c = "linha 1\nlinha 2\nlinha 3";
+	$a =~ s/^.*/!!/g; print "$a\n-------------\n";
+	$b =~ s/^.*/!!/gs; print "$b\n-------------\n";
+	$c =~ s/^.*/!!/gm; print "$c\n-------------\n";
 }
